99b65a5a46d3b2abee638485b8a05f1178288072
Shiva-shankarr <145248858+Shiva-shankarr@users.noreply.github.com>
2023-10-11 01:21:25 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+let arr=[10,20,30,40]
+let sum=arr.reduce((acc,ele)=>acc+ele)
+console.log("sum=",sum)
+let small=arr.reduce((acc,ele)=>acc<ele?acc:ele)
+console.log("sum=",small)
+
+
+//array of objects
+let student=[{},{},{},{}]
